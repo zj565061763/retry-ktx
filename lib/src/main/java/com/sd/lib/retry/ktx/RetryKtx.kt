31bd2a4a5569161c1a415c5f582ba345b918a052
zjun <565061763@qq.com>
2024-04-18 01:29:41 +0800

    comment

--- lib/src/main/java/com/sd/lib/retry/ktx/RetryKtx.kt
+++ lib/src/main/java/com/sd/lib/retry/ktx/RetryKtx.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.ensureActive
 /**
  * 执行重试逻辑，如果[block]发生异常，会延迟[interval]之后继续调用[block]重试，
  * 如果达到最大重试次数[maxCount]，则返回的[Result]异常为[FRetryExceptionRetryMaxCount]并携带最后一次失败的异常，
- * 注意：[block]发生的[CancellationException]异常不会被捕获
+ * 注意：[block]抛出的[CancellationException]异常不会被捕获
  */
 suspend fun <T> fRetry(
     /** 最多执行几次 */

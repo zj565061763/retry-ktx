966b78429b8b591b60c9710e54d7acfbd617b111
zjun <565061763@qq.com>
2024-05-01 20:03:36 +0800

    beforeBlock

--- lib/src/main/java/com/sd/lib/retry/ktx/Retry.kt
+++ lib/src/main/java/com/sd/lib/retry/ktx/Retry.kt
@@ -6,10 +6,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
 
 /**
- * 执行[block]，[block]发生的异常会被捕获并通知[onFailure]，[onFailure]的异常不会被捕获，
+ * 执行[block]，[block]发生的异常会被捕获([CancellationException]异常除外)并通知[onFailure]，[onFailure]的异常不会被捕获，
  * [block]发生异常之后，如果未达到最大执行次数[maxCount]，延迟[interval]之后继续执行[block]；
  * 如果达到最大执行次数[maxCount]，则返回的[Result]异常为[FRetryExceptionMaxCount]并携带最后一次的异常，
- * 注意：[block]抛出的[CancellationException]异常不会被捕获
+ * [beforeBlock]在[block]回调之前执行，[beforeBlock]发生的异常不会被捕获
  */
 suspend fun <T> fRetry(
     /** 最大执行次数 */
@@ -20,6 +20,9 @@ suspend fun <T> fRetry(
 
     /** 失败回调 */
     onFailure: FRetryScope.(Throwable) -> Unit = {},
+
+    /** 在[block]回调之前执行 */
+    beforeBlock: suspend FRetryScope.() -> Unit,
 
     /** 执行回调 */
     block: suspend FRetryScope.() -> T,
@@ -32,6 +35,10 @@ suspend fun <T> fRetry(
     while (true) {
         // 增加次数
         scope.increaseCount()
+
+        // before block
+        with(scope) { beforeBlock() }
+        currentCoroutineContext().ensureActive()
 
         // block
         val result = runCatching {

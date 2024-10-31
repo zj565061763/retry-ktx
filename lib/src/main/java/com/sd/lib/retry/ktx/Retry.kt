22cbe409ae0f5990cea206de765f29b625cf3876
zjun <565061763@qq.com>
2024-11-01 02:18:25 +0800

    comment

--- lib/src/main/java/com/sd/lib/retry/ktx/Retry.kt
+++ lib/src/main/java/com/sd/lib/retry/ktx/Retry.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.ensureActive
 
 /**
  * 执行[block]，[block]发生异常之后会被捕获并通知[onFailure] ([CancellationException]异常除外)，
- * 如果[onFailure]返回false则停止重试并返回失败结果；
+ * 如果[onFailure]返回false则停止执行并返回失败结果；
  * 如果[onFailure]返回true则继续执行后面的逻辑，如果未达到最大执行次数[maxCount]，延迟[getInterval]之后继续执行[block]；
  * 如果达到最大执行次数[maxCount]，则返回失败结果，异常为[RetryMaxCountException]并携带最后一次的异常。
  */
@@ -18,7 +18,7 @@ suspend inline fun <T> fRetry(
    /** 获取执行间隔(毫秒) */
    getInterval: RetryScope.() -> Long = { 5_000 },
 
-   /** 失败回调，返回false停止重试 */
+   /** 失败回调，返回false停止执行 */
    onFailure: RetryScope.(Throwable) -> Boolean = { true },
 
    /** 执行回调 */

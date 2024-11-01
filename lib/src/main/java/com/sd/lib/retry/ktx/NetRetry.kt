d93faa6e2b4d2575008762572a0c0f1d53e8ac00
zjun <565061763@qq.com>
2024-11-01 17:05:41 +0800

    shouldRetry

--- lib/src/main/java/com/sd/lib/retry/ktx/NetRetry.kt
+++ lib/src/main/java/com/sd/lib/retry/ktx/NetRetry.kt
@@ -1,5 +1,6 @@
 package com.sd.lib.retry.ktx
 
+import com.sd.lib.network.FNetwork
 import com.sd.lib.network.fNetwork
 import java.net.SocketTimeoutException
 
@@ -9,7 +10,7 @@ import java.net.SocketTimeoutException
 suspend inline fun <T> fNetRetry(
    maxCount: Int = 3,
    getDelay: RetryScope.() -> Long = { 5_000 },
-   onFailure: RetryScope.(Throwable) -> Boolean = { it is SocketTimeoutException },
+   onFailure: RetryScope.(Throwable) -> Boolean = { shouldRetry(it) },
    block: RetryScope.() -> T,
 ): Result<T> {
    return fRetry(
@@ -21,4 +22,10 @@ suspend inline fun <T> fNetRetry(
          block()
       },
    )
+}
+
+@PublishedApi
+internal fun shouldRetry(throwable: Throwable): Boolean {
+   if (throwable is SocketTimeoutException) return true
+   return !FNetwork.currentNetwork.isConnected()
 }
